1b288d427e42e12fbc60a57fe0d8be0b7a45ee54
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2025-03-22 22:32:06 +0800

    chore: types: CallbackMessage.Code 重新定义

--- server/Types.js
+++ server/Types.js
@@ -14,15 +14,19 @@ export class CallbackMessage {
         /**
          * 无权限
          */
-        static PERMISSION_DENIED = 4
+        static PERMISSION_DENIED = 401
+        /**
+         * 不存在
+         */
+         static NOT_FOUND = 404
         /**
          * 服务端错误
          */
-        static SERVER_ERROR = 5
+        static SERVER_ERROR = 500
         /**
          * 请求成功
          */
-        static OK = 2
+        static OK = 200
     }
     /**
      * @type { String }
