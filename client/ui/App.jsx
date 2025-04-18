576959b7a1296970bb8a88ffba0cc57804d0e0a1
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-04-18 23:24:01 +0800

    test: 测试消息组件

--- client/ui/App.jsx
+++ client/ui/App.jsx
@@ -1,7 +1,22 @@
+import Message from "./chat/Message.js"
+import MessageContainer from "./chat/MessageContainer.js"
+
 export default function App() {
     return (
         <div>
-            
+            {/* <MessageContainer>
+                <Message
+                    nickname="Fey"
+                    avatar="https://www.court-records.net/mugshot/aa6-004-maya.png">
+                    Test
+                </Message>
+                <Message
+                    direction="right"
+                    nickname="Fey"
+                    avatar="https://www.court-records.net/mugshot/aa6-004-maya.png">
+                    Test
+                </Message>
+            </MessageContainer> */}
         </div>
     )
 }
