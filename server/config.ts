9efd9ebd1df80ff6e20cb7ff85b72191e65fe2db
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-06-15 22:40:03 +0800

    chore: make lint happy, add database dir

--- server/config.ts
+++ server/config.ts
@@ -2,12 +2,13 @@ import io from './lib/io.js'
 
 export default class Config {
     static ensureAllDirsAreCreated() {
-        for (const key of Object.keys(Config.dirs)) {
+        for (const key of Object.keys(Config.dirs) as Array<keyof typeof Config.dirs>) {
             io.mkdirs(Config.dirs[key])
         }
     }
     static BASE_DIR = 'whitesilk_data'
-    static dirs : { [key: string]: string } = {
-        WEB_PAGE_DIR: this.BASE_DIR + '/_webpage'
+    static dirs = {
+        WEB_PAGE_DIR: this.BASE_DIR + '/_webpage',
+        DATABASES_DIR: this.BASE_DIR + '/databases',
     }
 }
