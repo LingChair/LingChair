c80fa5cd3c5d0c4f72b8cf0741abf09e30328b99
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-07-13 00:32:30 +0800

    User.ts: 更新 TypeScript 定義

--- server/data/User.ts
+++ server/data/User.ts
@@ -1,8 +1,12 @@
 // @ts-types="npm:sequelize"
-import { Sequelize, Model, DataTypes } from 'sequelize'
+import { Sequelize, Model, DataTypes, CreationOptional } from 'sequelize'
 
 export default class User extends Model {
-    declare created_id: number 
+    declare created_id: CreationOptional<number>
+    declare user_name: string | null
+    declare nick_name: string | null
+    declare avatar: Blob
+
     static async initTable(sequelize: Sequelize, name: string) {
         this.init({
             created_id: {
@@ -20,8 +24,8 @@ export default class User extends Model {
             },
             avatar: {
                 type: DataTypes.BLOB,
+                allowNull: true,
             },
-            
         }, {
             sequelize: sequelize,
             tableName: name,
