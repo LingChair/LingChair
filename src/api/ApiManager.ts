4a355101922f2baa2054a2f4a65d7817b0ed8864
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-08-16 16:07:24 +0800

    进一步完善 ApiManager

--- src/api/ApiManager.ts
+++ src/api/ApiManager.ts
@@ -1,12 +1,22 @@
 import BaseApi from './BaseApi.ts'
 import HttpServerLike from '../types/HttpServerLike.ts'
+import UserApi from "./UserApi.ts"
+import SocketIo from "socket.io"
 
 export default class ApiManager {
     static httpServer: HttpServerLike
-    static init(httpServer: HttpServerLike) {
+    static socketIoServer: SocketIo.Server
+    static apis_instance: {}
+    static init(httpServer: HttpServerLike, socketIoServer: SocketIo.Server) {
         this.httpServer = httpServer
+        this.socketIoServer = socketIoServer
     }
     static getHttpServer() {
         return this.httpServer
+    }
+    static initAllApis() {
+        this.apis_instance = {
+            user: new UserApi()
+        }
     }
 }
