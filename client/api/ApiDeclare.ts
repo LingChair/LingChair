7c679aa0d2beea872658f5cd5950c11f8918c9d5
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-05 20:39:23 +0800

    feat(wip): 進一步完善 Client 與服務端的交互邏輯

---
+++ client/api/ApiDeclare.ts
@@ -0,0 +1,3 @@
+export type CallMethod = 
+    "User.register" |
+    "User.login"
--- client/api/Client.ts
+++ client/api/Client.ts
@@ -1,10 +1,20 @@
 import { io, Socket } from 'https://unpkg.com/socket.io-client@4.8.1/dist/socket.io.esm.min.js'
+import { CallMethod } from './ApiDeclare.ts'
+import ApiCallbackMessage from './ApiCallbackMessage.ts'
 
 class Client {
     static socket: Socket
     static connect() {
         this.socket && this.socket.disconnect()
         this.socket = io()
+    }
+    static call(method: CallMethod, args: {}, timeout: number = 5000) {
+        return new Promise((resolve, reject) => {
+            this.socket.timeout().emit("The_White_Silk", (err, res: ApiCallbackMessage) => {
+                if (err) return reject(err)
+                
+            })
+        })
     }
 }
 
