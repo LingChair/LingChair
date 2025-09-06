2d92fffb5594f26b8299e3991721139630a0fc4b
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-06 16:40:49 +0800

    feat(wip): User & RecentChat in client

---
+++ client/api/client_data/RecentChat.ts
@@ -0,0 +1,6 @@
+export default class RecentChat {
+    declare id: string
+    declare title: string
+    declare avatar: string | null
+    declare content: string
+}
---
+++ client/api/client_data/User.ts
@@ -0,0 +1,7 @@
+export default class User {
+    declare id: string
+    declare count: number
+    declare username: string | null
+    declare nickname: string
+    declare avatar: string | null
+}
