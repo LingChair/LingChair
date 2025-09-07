abf06c71af45d2bfd1f8ddab1d2987934696167b
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-07 21:53:53 +0800

    chore: useEventListener allow Ref<null>

--- client/ui/useEventListener.ts
+++ client/ui/useEventListener.ts
@@ -1,8 +1,8 @@
 import * as React from 'react'
 
-export default function useEventListener<T extends HTMLElement>(ref: React.MutableRefObject<T>, eventName: string, callback: (event: Event) => void) {
+export default function useEventListener<T extends HTMLElement | null>(ref: React.MutableRefObject<T>, eventName: string, callback: (event: Event) => void) {
     React.useEffect(() => {
-        ref.current.addEventListener(eventName, callback)
-        return () => ref.current.removeEventListener(eventName, callback)
+        ref.current!.addEventListener(eventName, callback)
+        return () => ref.current!.removeEventListener(eventName, callback)
     }, [ref, eventName, callback])
 }
