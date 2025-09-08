316fd140bc279bb7e29bd78b528ebd95bef42114
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-08 23:16:41 +0800

    feat: BaseApi 兩個 Token 檢查方法

--- server/api/BaseApi.ts
+++ server/api/BaseApi.ts
@@ -1,6 +1,8 @@
 import EventCallbackFunction from "../typedef/EventCallbackFunction.ts"
 import ApiManager from "./ApiManager.ts"
 import { CallMethod } from './ApiDeclare.ts'
+import User from "../data/User.ts"
+import Token from "./Token.ts"
 
 export default abstract class BaseApi {
     abstract getName(): string
@@ -19,6 +21,16 @@ export default abstract class BaseApi {
             if (k in args && args[k] == '')
                 return true
         return false
+    }
+    checkUserToken(user: User, token: Token) {
+        if (!this.checkToken(token)) return false
+        if (token.author != user.bean.id) return false
+        return true
+    }
+    checkToken(token: Token) {
+        if (token.expired_time < Date.now()) return false
+        if (!User.findById(token.author)) return false
+        return true
     }
     registerEvent(name: CallMethod, func: EventCallbackFunction) {
         if (!name.startsWith(this.getName() + ".")) throw Error("注冊的事件應該與接口集合命名空間相匹配: " + name)
