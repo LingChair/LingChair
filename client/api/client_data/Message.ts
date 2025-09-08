bc1103489261f947d6b26f023950102bdb8362bd
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-08 23:18:13 +0800

    feat(wip): declare Message

---
+++ client/api/client_data/Message.ts
@@ -0,0 +1,5 @@
+export default class Message {
+    declare id: number
+    declare text: string
+    declare user_id: string
+}
---
+++ server/data/Message.ts
@@ -0,0 +1,7 @@
+export default class Message {
+    declare id: number
+    declare text: string
+    declare user_id: string
+
+    [key: string]: unknown
+}
