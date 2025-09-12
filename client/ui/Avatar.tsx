6896a1f8af9417fd20cec4d0eef8170d51bcb0b3
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-13 00:37:25 +0800

    refactor: Avatar.jsx -> .tsx

---
+++ client/ui/Avatar.tsx
@@ -0,0 +1,25 @@
+interface Args extends React.HTMLAttributes<HTMLElement> {
+    src?: string
+    text?: string
+    icon?: string
+    avatarRef?: React.LegacyRef<HTMLElement>
+}
+
+export default function Avatar({
+    src,
+    text,
+    icon = 'person',
+    avatarRef,
+    ...props
+}: Args) {
+    if (src != null)
+        return <mdui-avatar ref={avatarRef} {...props} src={src} />
+    else if (text != null)
+        return <mdui-avatar ref={avatarRef} {...props}>
+            {
+                text.substring(0, 0)
+            }
+        </mdui-avatar>
+    else
+        return <mdui-avatar icon={icon} ref={avatarRef} {...props} />
+}
