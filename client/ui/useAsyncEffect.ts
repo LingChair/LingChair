90295f0d38d7a01fd9766b0187d6e81b45a9bbc7
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-20 19:52:04 +0800

    fix: useAsyncEffect loops

--- client/ui/useAsyncEffect.ts
+++ client/ui/useAsyncEffect.ts
@@ -2,6 +2,7 @@ import React from "react"
 
 export default function useAsyncEffect(func: Function, deps?: React.DependencyList) {
     React.useEffect(() => {
-        ;(async () => await func())
-    }, deps)
+        func()
+        // 警告: 不添加 deps 有可能導致無限執行
+    }, deps || [])
 }
