fdf52c054840bd224192f427a6c4ed002ff7d88d
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-25 11:36:03 +0800

    feat(wip): 複製到剪貼薄

---
+++ client/ui/copyToClipboard.ts
@@ -0,0 +1,14 @@
+import { $ } from 'mdui/jq'
+
+export default function copyToClipboard(text: string) {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text)
+    } else {
+        const input = $('#copy_to_clipboard_fallback').get(0) as HTMLInputElement
+        input.value = text
+        input.select()
+        input.setSelectionRange(0, 1145141919810)
+        document.execCommand('copy')
+        input.clearSelection()
+    }
+}
