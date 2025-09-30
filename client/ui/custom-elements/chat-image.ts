7e8148493289fdafd49257fd920156fd08e32d90
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-09-30 21:37:31 +0800

    refactor: 獨立 openImageViewer

--- client/ui/custom-elements/chat-image.ts
+++ client/ui/custom-elements/chat-image.ts
@@ -1,23 +1,8 @@
+import openImageViewer from "../openImageViewer.ts"
+import { snackbar } from "../snackbar.ts"
+
 import { $ } from 'mdui/jq'
 
-import 'pinch-zoom-element'
-import { snackbar } from "../snackbar.ts";
-
-function openImageViewer(src: string) {
-    $('#image-viewer-dialog-inner').empty()
-
-    const e = new Image()
-    e.onload = () => ($('#image-viewer-dialog-inner').get(0) as any).scaleTo(0.1, {
-        // Transform origin. Can be a number, or string percent, eg "50%"
-        originX: '50%',
-        originY: '50%',
-        // Should the transform origin be relative to the container, or content?
-        relativeTo: 'container',
-    })
-    e.src = src
-    $('#image-viewer-dialog-inner').append(e)
-    $('#image-viewer-dialog').attr('open', 'true')
-}
 
 customElements.define('chat-image', class extends HTMLElement {
     constructor() {
