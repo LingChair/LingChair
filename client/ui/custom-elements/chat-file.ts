de886dcfcccb4f4865d300dae1c2259c2111bcda
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-10-02 21:27:52 +0800

    ui: 修正 chat-file 為 超鏈接

--- client/ui/custom-elements/chat-file.ts
+++ client/ui/custom-elements/chat-file.ts
@@ -7,16 +7,24 @@ customElements.define('chat-file', class extends HTMLElement {
     connectedCallback() {
         this.style.display = 'block'
         const e = new DOMParser().parseFromString(`
-        <div style="width: 100%;height: 100%;">
+        <a style="width: 100%;height: 100%;">
             <mdui-card variant="outlined" clickable style="display: flex;align-items: center;">
                 <mdui-icon name="insert_drive_file" style="margin: 13px;font-size: 34px;"></mdui-icon>
                 <span style="margin-right: 13px;"></span>
             </mdui-card>
-        </div>`, 'text/html').body.firstChild as HTMLElement
+        </a>`, 'text/html').body.firstChild as HTMLElement
         $(e).find('span').text($(this).attr("name"))
         const href = $(this).attr('href')
+        $(e).attr('href', href)
+        $(e).attr('target', '_blank')
+        $(e).attr('download', href)
+        e.style.textDecoration = 'none'
+        e.style.color = 'inherit'
         // deno-lint-ignore no-window
-        e.onclick = () => window.open(href, '_blank')
+        e.onclick = (e) => {
+            e.stopPropagation()
+            window.open(href, '_blank')
+        }
         this.appendChild(e)
     }
 })
