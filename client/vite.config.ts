c5ce13b13ce53a46732ae50021c41023f8af4e2b
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-10-03 23:33:06 +0800

    chore: 測試兼容更舊的瀏覽器内核 * 效果不咋地, Edge 84 因爲 Marked.js 炸了

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
-
+import pluginBabel from 'vite-plugin-babel'
 import config from '../server/config.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [deno(), react()],
+    plugins: [react(), pluginBabel({
+        babelConfig: {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            android: '70'
+                        },
+                    }
+                ],
+            ],
+        }
+    }), deno()],
     build: {
+        cssTarget: 'chrome70',
         sourcemap: true,
         outDir: "." + config.data_path + '/page_compiled',
     },
