f5f3774dafd919721a382c78340d3247228797e4
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-10-08 02:50:31 +0800

    chore: 移除了 babel 编译流程 * 加快调试速度 * 旧版本浏览器本来也没办法支持了...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,12 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
-import pluginBabel from 'vite-plugin-babel'
 import config from '../server/config.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), pluginBabel({
-        babelConfig: {
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        targets: {
-                            android: '70'
-                        },
-                    }
-                ],
-            ],
-        }
-    }), deno()],
+    plugins: [react(), deno()],
     build: {
-        cssTarget: 'chrome70',
         sourcemap: true,
         outDir: "." + config.data_path + '/page_compiled',
     },
